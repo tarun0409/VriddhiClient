e882279833f774d50e9e1099a10330af7427c6e3
Tarun Mohandas <tarun.mohandas@gmail.com>
2017-09-08 07:57:24 +0530

    item update done

--- js/page/controller/items.js
+++ js/page/controller/items.js
@@ -7,3 +7,26 @@ var getItemObjectFromUI = function(itemTypeId,itemNameId) {
     return itemObj;
 
 }
+
+var getItemUpdateObjectFromUI = function(itemTypeId,itemNameId) {
+      var itemObj = new Object();
+      if(itemTypeId!=null)
+      {
+          var typePh = localStorage.getItem("selectedItemType");
+          var typeValue = $(itemTypeId).val();
+          if(typeValue!=null && typeValue!="" && (typePh!=typeValue))
+          {
+              itemObj["Item Type"] = typeValue;
+          }
+      }
+      if(itemNameId!=null)
+      {
+          var namePh = $(itemNameId).attr('placeholder');
+          var nameValue = $(itemNameId).val();
+          if(nameValue!=null && nameValue!="" && (namePh!=typeValue))
+          {
+              itemObj["Item Name"] = nameValue;
+          }
+      }
+      return itemObj;
+}
--- js/page/item.js
+++ js/page/item.js
@@ -10,3 +10,13 @@ $(document).ready(function(){
 
 
 });
+
+var startUpdateProcess = function(itemSelectListId)
+{
+    var selectedItem = $(itemSelectListId).val();
+    if(selectedItem!=null && selectedItem.length==1)
+    {
+        localStorage.setItem("updateItemId",selectedItem[0]);
+        window.location.href = 'modals/ItemUpdate.html';
+    }
+}
